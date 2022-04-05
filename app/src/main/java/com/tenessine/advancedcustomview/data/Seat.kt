fda2432d8efbd155254e54af4e48a7365950e415
Catyousha <jakaasha@gmail.com>
2022-04-05 11:27:56 +0700

    HIBIKE-MUNE-NO-ITAMI-KOKORO

--- app/src/main/java/com/tenessine/advancedcustomview/data/Seat.kt
+++
@@ -1,9 +0,0 @@
-package com.tenessine.advancedcustomview.data
-
-data class Seat(
-    val id: Int,
-    var x: Float? = 0F,
-    var y: Float? = 0F,
-    var name: String,
-    var isBooked: Boolean
-)
