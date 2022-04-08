06efbdc215ac883a959b7a6ff56b2ceac88fa333
Catyousha <jakaasha@gmail.com>
2022-04-08 11:13:51 +0700

    Bokobokobo- Kobo Kanaeru at your service! Let me be your sun to shine your day, ehe~☔

--- IntoCameraX/app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
+++ IntoCameraX/app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
@@ -2,19 +2,25 @@ package com.dicoding.picodiploma.mycamera
 
 import android.Manifest
 import android.content.Intent
+import android.content.Intent.ACTION_GET_CONTENT
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.dicoding.picodiploma.mycamera.databinding.ActivityMainBinding
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
   private lateinit var binding: ActivityMainBinding
+  private lateinit var currentPhotoPath: String
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -41,15 +47,48 @@ class MainActivity : AppCompatActivity() {
   }
 
   private fun startGallery() {
-    Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+    // membuat intent untuk membuka galeri
+    // menentukan aksi dan tipe file yang akan dibuka
+    val intent = Intent().apply {
+      action = ACTION_GET_CONTENT
+      type = "image/*"
+    }
+
+    // membuat chooser untuk memilih file dengan intent yang telah dibuat
+    val chooser = Intent.createChooser(intent, "Choose image")
+
+    // memulai activity untuk memilih file
+    launcherIntentGallery.launch(chooser)
   }
 
   private fun startTakePhoto() {
-    Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+    // intent camera menggunakan fitur kamera yang sudah ada
+    val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+    // kemukakan activity apa yang memerintahkan intent ini
+    intent.resolveActivity(packageManager)
+
+    // membuat temp file untuk menyimpan gambar yang dicapture
+    createTempFile(application).also {
+      val photoURI: Uri =
+          FileProvider.getUriForFile(
+              this@MainActivity,
+              packageName,
+              it,
+          )
+      currentPhotoPath = it.absolutePath
+
+      // menambahkan file yang telah dibuat ke intent
+      intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+
+      // PROSES CAPTURE GAMBAR BERLANGSUNG DISINI
+      launcherIntentCamera.launch(intent)
+    }
+
   }
 
   private fun startCameraX() {
-    // beralih ke activiy camera
+    // cameraX membuat fitur kamera baru
     val intent = Intent(this, CameraActivity::class.java)
 
     // membawa intent dan mengharapkan result
@@ -83,6 +122,37 @@ class MainActivity : AppCompatActivity() {
         }
       }
 
+  private val launcherIntentCamera =
+      registerForActivityResult(
+          ActivityResultContracts.StartActivityForResult(),
+      ) {
+        if (it.resultCode == RESULT_OK) {
+          // dapatkan path yang ada
+          val myFile = File(currentPhotoPath)
+
+          // mengolah bitmap dari path
+          val result = BitmapFactory.decodeFile(myFile.path)
+
+          // tampilkan preview
+          binding.previewImageView.setImageBitmap(result)
+        }
+      }
+
+  private val launcherIntentGallery = registerForActivityResult(
+    ActivityResultContracts.StartActivityForResult(),
+  ) {
+    if (it.resultCode == RESULT_OK) {
+      // file yang dipilih didapatkan sebagai Uri
+      val selectedImg: Uri = it.data?.data as Uri
+
+      // set preview
+      binding.previewImageView.setImageURI(selectedImg)
+
+      // ubah uri menjadi file dengan bantuan Utils.kt
+      val myFile = uriToFile(selectedImg, this@MainActivity)
+    }
+  }
+
   // listener untuk hasil get permission
   override fun onRequestPermissionsResult(
       requestCode: Int,
@@ -106,6 +176,7 @@ class MainActivity : AppCompatActivity() {
 
   companion object {
     const val CAMERA_X_RESULT = 200
+    const val EXTRA_DATA = "data"
     private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     private const val REQUEST_CODE_PERMISSIONS = 10
   }
